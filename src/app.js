2817c4b13791fcfd79a3ac3723a753cea404f186
Lillian Ye <lillianye@princeton.edu>
2023-12-13 10:04:53 -0500

    start page to game

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ import *  as handlers from './handlers.js';
 
 import * as THREE from "three";
 import * as pages from "./pages.js";
+import './style.css';
 
 // Initialize core ThreeJS components
 const scene = new SeedScene();
